53e90bd7e2e94ef6ad3c3fb23f0ea012d2999f92
George Zhang <gzhang01@college.harvard.edu>
2016-02-16 14:02:50 -0500

    Implemented generation of MST

--- prog1/mst.c
+++ prog1/mst.c
@@ -9,36 +9,55 @@
 #include <stdlib.h>
 #include "creategraph.h"
 #include "node.h"
-#include "mst.h"
 
-void generateMST(int n, int d, edge *mst[n - 1]);
+void generateMST(int n, int d, edge *tree[n * (n - 1) / 2], edge *mst[n - 1]);
 void mergeSort(edge *tree[], int l, int r);
 void merge(edge *tree[], int l, int m, int r);
+void printMST(int n, edge *mst[n - 1]);
 
 // Generating a minimum spanning tree
 // Inputs:	n - number of vertices
 // 			d - dimension of vertex. if 0, then random edge lengths
+//          tree - complete graph of given number of vertices
 //			mst - pointer to array of edges making up MST
 // Output: no return value; generated tree stored in mst
-void generateMST(int n, int d, edge *mst[n - 1]) {
-	// Gets the entire starting tree
+// NOTE: tree is not preserved
+void generateMST(int n, int d, edge *tree[n * (n - 1) / 2], edge *mst[n - 1]) {
 	int edges = n * (n - 1) / 2;
-	edge *tree[edges];
-	generateGraph(n, d, tree);
     printGraph(n, tree);
 
+    // Sorts edges by weight
     mergeSort(tree, 0, edges - 1);
-    
-    printGraph(n, tree);
 
-    destroyGraph(n, tree);
+    // Initializes sets based on vertices
+    node *sets[n];
+    for (int i = 0; i < n; i++) {
+        sets[i] = makeSet(i);
+    }
+
+    // Find edges of MST
+    int foundEdges = 0;
+    for (int i = 0; i < n; i++) {
+        if (find(sets[tree[i]->p1]) != find(sets[tree[i]->p2])) {
+            mst[foundEdges] = tree[i];
+            join(sets[tree[i]->p1], sets[tree[i]->p2]);
+            foundEdges++;
+            if (foundEdges == n) {
+                break;
+            }
+        }
+    }
+
+    // Destroy all sets created
+    for (int i = 0; i < n; i++) {
+        destroySet(sets[i]);
+    }
 }
 
 // Sorts section of array with index in range [l, r)
 void mergeSort(edge *tree[], int l, int r) {
     // Find middle element, sort both halves, and merge back together
     if (l < r) {
-        printf("Here %i, %i\n", l, r);
         int m = l + (r - l) / 2;
         mergeSort(tree, l, m);
         mergeSort(tree, m + 1, r);
@@ -48,7 +67,6 @@ void mergeSort(edge *tree[], int l, int r) {
 
 // Merges halves back together
 void merge(edge *tree[], int l, int m, int r) {
-    printf("Merging %i, %i, %i\n", l, m, r);
     // Sizes of two halves
     int n1 = m - l + 1, n2 = r - m;
 
@@ -91,14 +109,30 @@ void merge(edge *tree[], int l, int m, int r) {
     }
 }
 
+// Prints out info about MST for testing
+// NOTE: Do not use with large values of n
+void printMST(int n, edge *mst[n - 1]) {
+    for (int i = 0; i < n; i++) {
+        printf("Edge from %i to %i with length %.4f\n", mst[i]->p1, mst[i]->p2, mst[i]->length);
+    }
+}
+
 int main(void) {
-	int n = 10;
+	int n = 5;
 	int d = 0;
-	edge *tree[n - 1];
+    int edges = n * (n - 1) / 2;
+    edge *tree[edges];
+    generateGraph(n, d, tree);
+	edge *mst[n - 1];
 
-	generateMST(n, d, tree);
+	generateMST(n, d, tree, mst);
 
-	// printGraph(n, graph);
+    printMST(n - 1, mst);
+
+
+
+    // Destroys graph
+    destroyGraph(n, tree);
 
 
 }
