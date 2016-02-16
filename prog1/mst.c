bde9093238734bc95485016e503ec18124ed53e0
George Zhang <gzhang01@college.harvard.edu>
2016-02-16 18:47:23 -0500

    moved merge sort temp arrays to heap to fix seg fault

--- prog1/mst.c
+++ prog1/mst.c
@@ -10,7 +10,7 @@
 #include "creategraph.h"
 #include "node.h"
 
-void generateMST(int n, int d, edge *tree[n * (n - 1) / 2], edge *mst[n - 1]);
+void generateMST(int n, int d, edge *graph[n * (n - 1) / 2], edge *mst[n - 1]);
 void mergeSort(edge *tree[], int l, int r);
 void merge(edge *tree[], int l, int m, int r);
 void printMST(int n, edge *mst[n - 1]);
@@ -22,12 +22,13 @@ void printMST(int n, edge *mst[n - 1]);
 //			mst - pointer to array of edges making up MST
 // Output: no return value; generated tree stored in mst
 // NOTE: tree is not preserved
-void generateMST(int n, int d, edge *tree[n * (n - 1) / 2], edge *mst[n - 1]) {
+void generateMST(int n, int d, edge *graph[n * (n - 1) / 2], edge *mst[n - 1]) {
 	int edges = n * (n - 1) / 2;
-    printGraph(n, tree);
+    // printGraph(n, graph);
 
     // Sorts edges by weight
-    mergeSort(tree, 0, edges - 1);
+    mergeSort(graph, 0, edges - 1);
+    // printGraph(n, graph);
 
     // Initializes sets based on vertices
     node *sets[n];
@@ -39,9 +40,9 @@ void generateMST(int n, int d, edge *tree[n * (n - 1) / 2], edge *mst[n - 1]) {
     int foundEdges = 0;
     for (int i = 0; i < edges; i++) {
         // If roots are not the same, join them and add edge to MST
-        if (find(sets[tree[i]->p1]) != find(sets[tree[i]->p2])) {
-            mst[foundEdges] = tree[i];
-            join(sets[tree[i]->p1], sets[tree[i]->p2]);
+        if (find(sets[graph[i]->p1]) != find(sets[graph[i]->p2])) {
+            mst[foundEdges] = graph[i];
+            join(sets[graph[i]->p1], sets[graph[i]->p2]);
             foundEdges++;
             // Break if we've found n - 1 edges (num edges in MST)
             if (foundEdges == n - 1) {
@@ -57,40 +58,42 @@ void generateMST(int n, int d, edge *tree[n * (n - 1) / 2], edge *mst[n - 1]) {
 }
 
 // Sorts section of array with index in range [l, r)
-void mergeSort(edge *tree[], int l, int r) {
+void mergeSort(edge *graph[], int l, int r) {
     // Find middle element, sort both halves, and merge back together
     if (l < r) {
         int m = l + (r - l) / 2;
-        mergeSort(tree, l, m);
-        mergeSort(tree, m + 1, r);
-        merge(tree, l, m, r);
+        mergeSort(graph, l, m);
+        mergeSort(graph, m + 1, r);
+        merge(graph, l, m, r);
     }
 }
 
 // Merges halves back together
-void merge(edge *tree[], int l, int m, int r) {
+void merge(edge *graph[], int l, int m, int r) {
     // Sizes of two halves
     int n1 = m - l + 1, n2 = r - m;
 
     // Temp arrays to hold halves
-    edge *left[n1], *right[n2];
+    edge **left = malloc(n1 * sizeof(edge*));
+    edge **right = malloc(n2 * sizeof(edge*));
 
     // Copy halves of array into temp arrays
     for (int i = 0; i < n1; i++) {
-        left[i] = tree[l + i];
+        left[i] = graph[l + i];
     }
+
     for (int i = 0; i < n2; i++) {
-        right[i] = tree[m + 1 + i];
+        right[i] = graph[m + 1 + i];
     }
 
     // Merging halves back together
     int i = 0, j = 0, k = l;
     while (i < n1 && j < n2) {
         if (left[i]->length < right[j]->length) {
-            tree[k] = left[i];
+            graph[k] = left[i];
             i++;
         } else {
-            tree[k] = right[j];
+            graph[k] = right[j];
             j++;
         }
         k++;
@@ -98,17 +101,20 @@ void merge(edge *tree[], int l, int m, int r) {
 
     // Adding in rest of left list, if any are left
     while (i < n1) {
-        tree[k] = left[i];
+        graph[k] = left[i];
         i++;
         k++;
     }
 
     // Adding in rest of right list, if any are left
     while (j < n2) {
-        tree[k] = right[j];
+        graph[k] = right[j];
         j++;
         k++;
     }
+
+    free(left);
+    free(right);
 }
 
 // Prints out info about MST for testing
@@ -125,22 +131,28 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // Obtain info from command line
+    // int flag = (int) strtol(argv[1], NULL, 10);
 	int n = (int) strtol(argv[2], NULL, 10);
-	int d = (int) strtol(argv[4], NULL, 10);
+	// int trials = (int) strtol(argv[3], NULL, 10);
+    int d = (int) strtol(argv[4], NULL, 10);
 
+    // Space for complete graph and MST
     int edges = n * (n - 1) / 2;
-    edge *tree[edges];
-    generateGraph(n, d, tree);
+    edge *graph[edges];
 	edge *mst[n - 1];
 
-	generateMST(n, d, tree, mst);
+    // Create graph and MST
+    generateGraph(n, d, graph);
+	generateMST(n, d, graph, mst);
 
-    printMST(n - 1, mst);
+
+    // printMST(n - 1, mst);
 
 
 
     // Destroys graph
-    destroyGraph(n, tree);
+    destroyGraph(n, graph);
 
 
 }
