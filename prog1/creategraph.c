c9dde7ab8905a88f5c43e36d8b532451b2c7e9a2
George Zhang <gzhang01@college.harvard.edu>
2016-02-17 19:17:51 -0500

    Implemented pruning of graph based on threshold

--- prog1/creategraph.c
+++ prog1/creategraph.c
@@ -13,39 +13,42 @@
 
 #define INDEX(i, j) (i - 1) * i / 2 + j
 
-void generateGraph(int n, int d, edge *graph[n * (n - 1) / 2]);
-void destroyGraph(int n, edge *graph[n * (n - 1) / 2]);
+int generateGraph(int n, int d, edge *graph[n * (n - 1) / 2]);
+void destroyGraph(int edges, edge *graph[]);
 void printGraph(int n, edge *graph[n * (n - 1) / 2]);
-void generate0d(int n, edge *graph[n * (n - 1) / 2]);
-void generate234d(int n, int d, edge *graph[n * (n - 1) / 2]);
+int generate0d(int n, float th, edge *graph[n * (n - 1) / 2]);
+int generate234d(int n, int d, float th, edge *graph[n * (n - 1) / 2]);
 float randFloat();
 float distance(int d, float p1[d], float p2[d]);
 float square(float n);
+float threshold(int n, int d);
 
 // Generates a graph with n vertices in adjacency matrix form
 // Input: 	n - number of vertices
 // 			d - dimension of vertex. if 0, then random edge lengths
 //			graph - pointer to edge array containing all edges in graph
-// Output: no return value; generated graph stored in graph
-void generateGraph(int n, int d, edge *graph[n * (n - 1) / 2]) {
+// Output: returns number of edges created; generated graph stored in graph
+int generateGraph(int n, int d, edge *graph[n * (n - 1) / 2]) {
 	// initialize RNG with microsecond
     struct timeval tv;
     gettimeofday(&tv, NULL);
 	srand((unsigned) tv.tv_usec);
 
+    // Obtain threshold
+    float th = threshold(n, d);
+
 	// Generate graph
 	if (d == 0) {
-		generate0d(n, graph);
+		return generate0d(n, th, graph);
 	} else {
-		generate234d(n, d, graph);
+		return generate234d(n, d, th, graph);
 	}
 }
 
 // Frees all memory allocated in graph
-// Input:   n - number of vertices
+// Input:   edges - number of edges
 //          graph - array of (n C 2) edges
-void destroyGraph(int n, edge *graph[n * (n - 1) / 2]) {
-    int edges = n * (n - 1) / 2;
+void destroyGraph(int edges, edge *graph[]) {
     for (int i = 0; i < edges; i++) {
         free(graph[i]);
     }
@@ -54,7 +57,7 @@ void destroyGraph(int n, edge *graph[n * (n - 1) / 2]) {
 // Pretty prints the graph to stdout for testing
 // Prints in matrix form (bottom triangular)
 // NOTE: should not be used for large values of n!
-// Input:   n - number of vertices
+// Input:   edges - number of edges
 //          graph - array of (n C 2) edges
 // Example: let n = 3
 // Output:
@@ -67,7 +70,7 @@ void destroyGraph(int n, edge *graph[n * (n - 1) / 2]) {
 // 0.2 = weight(0,3)
 // 0.4 = weight(1,3)
 // 0.6 = weight(2,3)
-void printGraph(int n, edge *graph[n * (n - 1) / 2]) {
+void printGraph(int edges, edge *graph[]) {
     int edges = n * (n - 1) / 2;
     int count = 0, row = 1;
     for (int i = 0; i < edges; i++) {
@@ -84,23 +87,31 @@ void printGraph(int n, edge *graph[n * (n - 1) / 2]) {
 
 // Helper function
 // Creates graph of n vertices, where weight of each edge is in [0, 1]
-// Stores result in graph
-void generate0d(int n, edge *graph[n * (n - 1) / 2]) {
+// Stores result in graph; returns number of edges added
+int generate0d(int n, float th, edge *graph[n * (n - 1) / 2]) {
+    int numEdges = 0;
     // Find all edges
     for (int i = 1; i < n; i++) {
         for (int j = 0; j < i; j++) {
+            // If value is greater than threshold, ignore it
+            float val = randFloat();
+            if (val > th) {
+                continue;
+            }
             edge *e = malloc(sizeof(edge));
             e->p1 = i;
             e->p2 = j;
-            e->length = randFloat();
-            graph[INDEX(i, j)] = e;
+            e->length = val;
+            graph[numEdges] = e;
+            numEdges++;
         }
     }
+    return numEdges;
 }
 
 // Helper function
 // Generates graph for n vertices initialized in d dimensions
-void generate234d(int n, int d, edge *graph[n * (n - 1) / 2]) {
+int generate234d(int n, int d, float th, edge *graph[n * (n - 1) / 2]) {
 	// Keep track of vertices. Each row is single vertex with dimensionality d
 	float vertices[n][d];
 
@@ -111,16 +122,33 @@ void generate234d(int n, int d, edge *graph[n * (n - 1) / 2]) {
 		}
 	}
 
+    int numEdges = 0;
+
     // Find all edges
     for (int i = 1; i < n; i++) {
         for (int j = 0; j < i; j++) {
+            // If individual axes difference greater than threshold, ignore
+            for (int dim = 0; dim < d; dim++) {
+                if (vertices[i][dim] - vertices[j][dim] > th) {
+                    continue;
+                }
+            }
+
+            // If distance between points greather than threshold, ignore
+            float dist = distance(d, vertices[i], vertices[j]);
+            if (dist > th) {
+                continue;
+            }
+
             edge *e = malloc(sizeof(edge));
             e->p1 = i;
             e->p2 = j;
-            e->length = distance(d, vertices[i], vertices[j]);
-            graph[INDEX(i, j)] = e;
+            e->length = dist;
+            graph[numEdges] = e;
+            numEdges++;
         }
     }
+    return numEdges;
 }
 
 // Generates a random float from 0 to 1
@@ -143,4 +171,25 @@ float distance(int d, float p1[d], float p2[d]) {
 float square(float n) {
 	return n * n;
 }
+
+// Calculates a threshold given n vertices and dimension d
+// Values determined by experimental data
+float threshold(int n, int d) {
+    if (d == 0) {
+        return 3.5 / (0.16799676 * n + 2.93108083);
+    } else if (d == 2) {
+        return 2.5 / (0.03520531 * n + 2.6599906);
+    } else if (d == 3) {
+        return 2.0 / (0.01392143 * n + 2.03798067);
+    } else if (d == 4) {
+        return 1.8 / (0.00761201 * n + 1.65815953);
+    }
+
+    // If given d is not valid, return -1 as error
+    return -1;
+}
+
+
+
+
 
--- prog1/creategraph.h
+++ prog1/creategraph.h
@@ -15,17 +15,17 @@ typedef struct {
 // Input: 	n - number of vertices
 // 			d - dimension of vertex. if 0, then random edge lengths
 //			graph - pointer to edge array containing all edges in graph
-// Output: no return value; generated graph stored in graph
-void generateGraph(int n, int d, edge* graph[n * (n - 1) / 2]);
+// Output: returns number of edges created; generated graph stored in graph
+int generateGraph(int n, int d, edge* graph[n * (n - 1) / 2]);
 
 // Frees all memory allocated in graph
-// Input:   n - number of vertices
+// Input:   edges - number of edges
 //          graph - array of (n C 2) edges
-void destroyGraph(int n, edge* graph[n * (n - 1) / 2]);
+void destroyGraph(int edges, edge* graph[]);
 
 // Pretty prints the graph to stdout for testing
 // Prints in matrix form (bottom triangular)
 // NOTE: should not be used for large values of n!
-// Input:   n - number of vertices
+// Input:   edges - number of edges
 //          graph - array of (n C 2) edges
-void printGraph(int n, edge* graph[n * (n - 1) / 2]);
+void printGraph(int n, edge* graph[]);
