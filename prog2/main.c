03dc77c3792e1efb7dce72e0215b65f39c0554c5
George Zhang <gzhang01@college.harvard.edu>
2016-03-20 18:52:09 -0400

    Satisfies input/output requirements

--- prog2/main.c
+++ prog2/main.c
@@ -4,50 +4,61 @@
  *	Runs the implementation of modified Strassen's algorithm
  *
  *	@author: George Zhang
+ *  @author: Daniel Chen
  *	Harvard University
  *	CS124 Prog 2
  *
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include "matrix.h"
 #include "matrixMultiplication.h"
 
-int main(void) {
-	// Testing matrix multiplication
-	// int row = 2;
-	// int mid = 3;
-	// int col = 2;
+int main(int argc, char* argv[]) {
+	if (argc != 4) {
+		printf("Usage: ./strassen 0 dimension inputfile\n");
+		return 1;
+	}
 
-	// matrix* m1 = generateRandomMatrix(row, mid);
-	// matrix* m2 = generateRandomMatrix(mid, col);
-	// printMatrix(m1);
-	// printMatrix(m2);
+	// Threshold
+	int t = 1;
 
-	// matrix* p = createMatrix(row, col);
+	// Get dimensions, padding and create matrices
+	int dim = (int) strtol(argv[2], NULL, 10);
+	int pad = dim;
+	matrix* mtx[2];
+	mtx[0] = createMatrix(pad);
+	mtx[1] = createMatrix(pad);
+
+	// Populate matrices
+	FILE* f = fopen(argv[3], "r");
+	if (f == NULL) {
+		printf("Could not open file\n");
+		return 2;
+	}
+
+	char buf[11];
+	for (int i = 0, n = dim * dim; i < 2 * n; i++) {
+		fscanf(f, "%s\n", buf);
+		setElement(mtx[i / n], (i % n) / dim, i % dim, (int) strtol(buf, NULL, 10));
+	}
+
+	fclose(f);
+
+	// Compute matrix product
+	matrix* p = createMatrix(pad);
+	matrixMult(p, mtx[0], mtx[1], t);
+
+	for (int i = 0; i < dim; i++) {
+		printf("%i\n", getElement(p, i, i));
+	}
+
+
+
+	
 	// time_t start = time(NULL);
-	// matrixMultiplicationStandard(p, m1, m2);
 	// time_t end = time(NULL);
-	// printMatrix(p);
-
 	// printf("Total time: %ld\n", end - start);
-
-	// freeMatrix(m1);
-	// freeMatrix(m2);
-	// freeMatrix(p);
-
-
-	// Testing splitting matrices
-	int dim = 2;
-	matrix* m1 = generateRandomMatrix(dim);
-	matrix* m2 = generateRandomMatrix(dim);
-	matrix* p = createMatrix(dim);
-	matrixMultiplicationStrassen(p, m1, m2);
-	printMatrix(m1);
-	printMatrix(m2);
-	printMatrix(p);
-	freeMatrix(m1);
-	freeMatrix(m2);
-	freeMatrix(p);
 }
