3db76b05fdbc55feaba5e55ba4e814f530f11baf
George Zhang <gzhang01@college.harvard.edu>
2016-04-19 14:34:28 -0400

    Graphs of results made

--- prog3/plotResidue.py
+++ prog3/plotResidue.py
@@ -9,27 +9,28 @@ with open("data.csv", "r") as f:
 x = []
 y = []
 shift = {0:0, 1:1, 3:2, 5:3, 7:4, 2:5, 4:6, 6:7, 8:8}
-labels = ['KK', 
-	'R (S)', 
-	'R (P)', 
-	'RR (S)', 
-	'RR (P)', 
-	'HC (S)',
-	'HC (P)',
-	'SA (S)',
-	'SA (P)']
-
+labels = ['KK', 'R (S)', 'R (P)', 'RR (S)', 'RR (P)', 'HC (S)','HC (P)','SA (S)','SA (P)']
+colors = []
 for i in xrange(len(data)):
 	for j in xrange(len(data[i])):
 		x.append(shift[j])
 		y.append(int(data[i][j]))
+		if shift[j] in [0]:
+			colors.append("red")
+		elif shift[j] in [1, 2, 3, 4]:
+			colors.append("blue")
+		elif shift[j] in [5, 6, 7, 8]:
+			colors.append("green")
 
 x = np.array(x)
 y = np.array(y)
 
-plt.subplot(1,1,1)
-plt.scatter(x, y)
+splt = plt.subplot(1,1,1)
+splt.set_xlabel('Heuristics')
+splt.set_ylabel('Residue')
+plt.scatter(x, y, color=colors)
 plt.xticks(x, labels)
 plt.ylim([1,100000000000000])
 plt.yscale('log')
+plt.savefig('residues.png')
 plt.show()
---
+++ prog3/plotTime.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = []
+with open("times.csv", "r") as f:
+	for line in f:
+		data.append(line[:-1].split(","))
+
+x = []
+y = []
+shift = {0:-1, 1:-1, 3:0, 5:1, 7:2, 2:-1, 4:3, 6:4, 8:5}
+labels = ['RR (S)', 'RR (P)', 'HC (S)','HC (P)','SA (S)','SA (P)']
+colors = []
+for i in xrange(len(data)):
+	for j in xrange(len(data[i])):
+		if j in [0, 1, 2]:
+			continue
+		assert(shift[j] != -1)
+		x.append(shift[j])
+		y.append(int(data[i][j]))
+		if shift[j] in [0, 1, 2]:
+			colors.append("blue")
+		elif shift[j] in [3, 4, 5]:
+			colors.append("green")
+
+x = np.array(x)
+y = np.array(y)
+
+splt = plt.subplot(1,1,1)
+splt.set_xlabel('Heuristic')
+splt.set_ylabel('Time (in ms)')
+plt.scatter(x, y, color=colors)
+plt.xticks(x, labels)
+plt.ylim([10,2500])
+plt.yscale('log')
+plt.savefig('times.png')
+plt.show()
